9b4496205e8c340868477a54654c54d8d194e879
GG-Satya <dsatyaprasanna@gmail.com>
2022-12-14 09:40:27 +0530

    14th December 2022, Daily LeetCode Challenge

---
+++ 198. House Robber/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        int rob = 0; 
+
+        int notrob = 0; 
+
+        for(int i=0; i<nums.length; i++) {
+
+        	int currob = notrob + nums[i]; 
+
+        	notrob = Math.max(notrob, rob); 
+
+        	rob = currob;
+    	  }
+    return Math.max(rob, notrob);
+    }
+}
