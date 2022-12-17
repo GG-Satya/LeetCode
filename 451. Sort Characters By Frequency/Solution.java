ed37e9ff8afa9768a7d77c9877ea1e3847fe50a6
GG-Satya <dsatyaprasanna@gmail.com>
2022-12-17 14:43:04 +0530

    2nd December 2022, Daily LeetCode Challenge

--- 451. Sort Characters By Frequency/Solution.java
+++ 451. Sort Characters By Frequency/Solution.java
@@ -1,28 +1,26 @@
-
 class Solution {
-
     public String frequencySort(String s) {
 
+        //storing all frequencies of char
         Map<Character, Integer> map = new HashMap<>();
-        for (char c : s.toCharArray()) 
+
+        for (char c : s.toCharArray()) {
+
             map.put(c, map.getOrDefault(c, 0) + 1);
-						
-        List<Character> [] bucket = new List[s.length() + 1];
-
-        for (char key : map.keySet()) {
-            int frequency = map.get(key);
-            if (bucket[frequency] == null) bucket[frequency] = new ArrayList<>();
-            bucket[frequency].add(key);
         }
-				
-        StringBuilder sb = new StringBuilder();
+        			
+	 // sorting the frequencies in descending order
+	 List<Character> list = new ArrayList<Character>(map.keySet());
+	 Collections.sort(list, (a,b) -> (map.get(b)-map.get(a)));
+        
 
-        for (int pos = bucket.length - 1; pos >= 0; pos--)
-            if (bucket[pos] != null)
-                for (char c : bucket[pos])
-                    for (int i = 0; i < pos; i++)
-                        sb.append(c);
-
-        return sb.toString();
+	 // buiding string accoring to the sorted frequency list
+	 StringBuilder sb = new StringBuilder();
+	 for(char c : list) {
+		 for(int i=0; i<map.get(c); i++) {
+			 sb.append(c);
+		 }
+	 }
+	 return sb.toString();
     }
 }
