12f054b1b3fc0499b8e32aec135f42884fbec6b9
GG-Satya <dsatyaprasanna@gmail.com>
2022-12-20 08:58:23 +0530

    20th December 2022, Daily LeetCode Challenge

---
+++ 841. Keys and Rooms/Solution.java
@@ -0,0 +1,25 @@
+					// 841. Keys and Rooms (Solution)
+class Solution {
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+
+        Stack<Integer> dfs = new Stack<>(); dfs.add(0);
+
+        HashSet<Integer> seen = new HashSet<Integer>(); seen.add(0);
+
+        while (!dfs.isEmpty()) {
+
+            int i = dfs.pop();
+
+            for (int j : rooms.get(i))
+
+                if (!seen.contains(j)) {
+                    dfs.add(j);
+                    seen.add(j);
+
+                    if (rooms.size() == seen.size()) return true;
+                }
+        }
+        return false;   
+    }
+}
