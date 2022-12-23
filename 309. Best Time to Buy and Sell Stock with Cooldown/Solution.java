9f637e5371e34d6d145a68d5ac417d30f96f4d1d
GG-Satya <dsatyaprasanna@gmail.com>
2022-12-23 09:31:10 +0530

    23rd December 2022, Daily LeetCode Challenge

---
+++ 309. Best Time to Buy and Sell Stock with Cooldown/Solution.java
@@ -0,0 +1,22 @@
+// 309. Best Time to Buy and Sell Stock with Cooldown
+// ==================================================
+
+class Solution {
+
+    public int maxProfit(int[] prices) {
+
+        int sell = 0, prev_sell = 0, buy = Integer.MIN_VALUE, prev_buy;
+
+        for (int price : prices) {
+
+            prev_buy = buy;
+
+            buy = Math.max(prev_sell - price, prev_buy);
+
+            prev_sell = sell;
+
+            sell = Math.max(prev_buy + price, prev_sell);
+        }
+        return sell;
+    }
+}
