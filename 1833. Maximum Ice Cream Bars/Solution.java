4730e38d04e9d4bbacf49291f71b203528b90e6a
GG-Satya <dsatyaprasanna@gmail.com>
2023-01-06 09:07:43 +0530

    6th January 2023, Daily LeetCode Challenge

---
+++ 1833. Maximum Ice Cream Bars/Solution.java
@@ -0,0 +1,16 @@
+// 1833. Maximum Ice Cream Bars
+// ============================
+
+class Solution {
+
+    public int maxIceCream(int[] costs, int coins) {
+
+    Arrays.sort(costs);
+
+    for (int i = 0; i < costs.length; ++i)
+
+        if ((coins -= costs[i]) < 0) return i;
+
+    return costs.length;
+    }
+}
