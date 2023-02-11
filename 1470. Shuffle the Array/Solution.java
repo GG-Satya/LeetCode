7a942f3e5909e7215f8c48c6f21036c834f9eac6
GG-Satya <dsatyaprasanna@gmail.com>
2023-02-11 10:57:04 +0530

    6th February 2023, Daily LeetCode Challenge

---
+++ 1470. Shuffle the Array/Solution.java
@@ -0,0 +1,26 @@
+// 1470. Shuffle the Array
+// =======================
+
+class Solution {
+
+    public int[] shuffle(int[] nums, int n) {
+        
+        int len = nums.length;
+        
+        for(int i = n; i < len; i++) {
+
+            nums[i] = (nums[i] << 10) | nums[i - n];
+        }
+        
+        int index = 0;
+
+        for(int i = n; i < len; i++, index += 2) {
+
+            nums[index] = nums[i] & 1023;
+
+            nums[index + 1] = nums[i] >>> 10;
+        }
+        
+        return nums;
+    }
+}
