6c3ec56d1a0ed70d232d788047d4b136dd0e1648
GG-Satya <dsatyaprasanna@gmail.com>
2023-02-16 09:21:35 +0530

    15th February 2023, Daily LeetCode Challenge

---
+++ 989. Add to Array-Form of Integer/Solution.java
@@ -0,0 +1,25 @@
+// 989. Add to Array-Form of Integer
+// =================================
+
+class Solution {
+
+    public List<Integer> addToArrayForm(int[] num, int k) {
+
+        List<Integer> res = new LinkedList<>();
+
+        for (int i = num.length - 1; i >= 0; --i) {
+
+            res.add(0, (num[i] + k) % 10);
+
+            k = (num[i] + k) / 10;
+        }
+
+        while (k > 0) {
+
+            res.add(0, k % 10);
+
+            k /= 10;
+        }
+        return res;
+    }
+}
