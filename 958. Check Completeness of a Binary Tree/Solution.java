3aaba53b60db33400431ce088930629ec054677c
GG-Satya <dsatyaprasanna@gmail.com>
2023-03-15 08:00:57 +0530

    15th March 2023, Daily LeetCode Challenge

---
+++ 958. Check Completeness of a Binary Tree/Solution.java
@@ -0,0 +1,44 @@
+// 958. Check Completeness of a Binary Tree
+// ========================================
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+class Solution {
+
+    public boolean isCompleteTree(TreeNode root) {
+
+       Queue<TreeNode> bfs = new LinkedList<TreeNode>();
+
+        bfs.offer(root);
+
+        while (bfs.peek() != null) {
+
+            TreeNode node = bfs.poll();
+
+            bfs.offer(node.left);
+
+            bfs.offer(node.right);
+        }
+        while (!bfs.isEmpty() && bfs.peek() == null)
+
+            bfs.poll();
+
+        return bfs.isEmpty();
+    }
+}
