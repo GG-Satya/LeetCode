6c8fbeb33c03297d6ad9dc8ffa3c0bb51b4269c2
GG-Satya <dsatyaprasanna@gmail.com>
2023-03-25 10:34:29 +0530

    21th March 2023, Daily LeetCode Challenge

---
+++ 2348. Number of Zero-Filled Subarrays/Solution.java
@@ -0,0 +1,16 @@
+// 2348. Number of Zero-Filled Subarrays
+// =====================================
+
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long cnt = 0, zeroSubarraysEndingAtCurrentIndex = 0;
+        for (int n : nums) {
+            if (n == 0) {
+                cnt += ++zeroSubarraysEndingAtCurrentIndex;
+            }else {
+                zeroSubarraysEndingAtCurrentIndex = 0;
+            }
+        }
+        return cnt;
+    }
+}
