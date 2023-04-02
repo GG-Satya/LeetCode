09dc87c3c345c5b5afc9445dd6bea16a2bf8072a
GG-Satya <dsatyaprasanna@gmail.com>
2023-04-02 20:25:23 +0530

    1st April 2023, Daily LeetCode Challenge

---
+++ 704. Binary Search/Solution.java
@@ -0,0 +1,15 @@
+// 704. Binary Search
+// ===================
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int i = 0, j = nums.length-1;
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(target == nums[mid])return mid;
+            else if(target < nums[mid]) j = mid -1;
+            else i = mid + 1;
+        }
+        return -1;
+    }
+}
