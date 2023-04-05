233e94707de5bd58bd93c2cbe73c5916ee767f33
GG-Satya <dsatyaprasanna@gmail.com>
2023-04-05 09:07:00 +0530

    5th April 2023, Daily LeetCode Challenge

---
+++ 2439. Minimize Maximum of Array/Solution.java
@@ -0,0 +1,13 @@
+// 2439. Minimize Maximum of Array
+// ===============================
+
+class Solution {
+    public int minimizeArrayValue(int[] nums) {
+        long sum = 0, res = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            sum += nums[i];
+            res = Math.max(res, (sum + i) / (i + 1));
+        }
+        return (int)res;
+    }
+}
