83c045d04e790be41397754c099827293325cc0d
GG-Satya <dsatyaprasanna@gmail.com>
2023-04-24 09:22:13 +0530

    24th April 2023, Daily LeetCode Challenge

---
+++ 1046. Last Stone Weight/Solution.java
@@ -0,0 +1,13 @@
+// 1046. Last Stone Weight
+// =======================
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)-> b - a);
+        for (int a : stones)
+            pq.offer(a);
+        while (pq.size() > 1)
+            pq.offer(pq.poll() - pq.poll());
+        return pq.poll();
+    }
+}
