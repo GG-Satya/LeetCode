87eb45c84c59386e18b9a7fe7e03033799dbcd88
GG-Satya <dsatyaprasanna@gmail.com>
2023-05-12 08:24:25 +0530

    12th May 2023, Daily LeetCode Challenge

---
+++ 2140. Solving Questions With Brainpower/Solution.java
@@ -0,0 +1,16 @@
+// 2140. Solving Questions With Brainpower
+// =======================================
+
+class Solution {
+        long dp[];
+    public long mostPoints(int[][] questions) {
+        dp = new long[questions.length];
+        return dfs(questions, 0);
+    }
+    public long dfs(int[][] Q, int i) {
+        if (i >= Q.length) return 0;
+        if (dp[i] > 0) return dp[i];
+        int points = Q[i][0], jump = Q[i][1];
+        return dp[i] = Math.max(dfs(Q, i + 1), points + dfs(Q, i + jump + 1));
+    }
+}
