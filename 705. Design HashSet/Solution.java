a6e2476d004f0dc4318974fdb5607550eec323ee
GG-Satya <dsatyaprasanna@gmail.com>
2023-05-30 09:28:07 +0530

    30th May 2023, Daily LeetCode Challenge

---
+++ 705. Design HashSet/Solution.java
@@ -0,0 +1,69 @@
+// 705. Design HashSet
+// ===================
+
+class MyHashSet {
+    List<Integer>[] container;
+    int cap = 1000;
+    double loadFactor = 0.75;
+    int count = 0; 
+    public MyHashSet() {
+        container = new LinkedList[cap];
+    }
+    
+    public void add(int key) {
+        if(contains(key)) return;
+        if(loadFactor*cap == count){
+            count = 0;
+            //rehash
+            cap *= 2;
+            List<Integer>[] oldC = container;
+            container = new LinkedList[cap];
+            for(int i=0; i < oldC.length; i++){
+                List<Integer> list = oldC[i];
+                if(list != null){
+                    for(int entry : list)
+                       this.add(entry); 
+                }
+            }
+        }
+        int hash = key % cap;
+        if(container[hash] == null)
+            container[hash] = new LinkedList<>();
+        container[hash].add(key);
+        ++count;
+    }
+    
+    public void remove(int key) {
+       int hash = key % cap;
+        List<Integer> list = container[hash];
+        if(list != null){
+            Iterator<Integer> itr = list.iterator();
+            while(itr.hasNext())
+                if(itr.next() == key){
+                    itr.remove();
+                    --count;
+					break;
+                }
+        }
+    }
+    
+    public boolean contains(int key) {
+        int hash = key % cap;
+        List<Integer> list = container[hash];
+        if(list != null){
+            Iterator<Integer> itr = list.iterator();
+            while(itr.hasNext())
+                if(itr.next() == key)
+                    return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
