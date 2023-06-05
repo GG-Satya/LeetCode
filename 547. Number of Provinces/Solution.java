4f13e37f692834688f410167ed32f55ec7112ddb
GG-Satya <dsatyaprasanna@gmail.com>
2023-06-05 09:35:49 +0530

    4th june 2023, Daily LeetCode Challenge

---
+++ 547. Number of Provinces/Solution.java
@@ -0,0 +1,24 @@
+// 547. Number of Provinces
+// ========================
+
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int[] visited = new int[isConnected.length];
+        int count = 0;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (visited[i] == 0) {
+                dfs(isConnected, visited, i);
+                count++;
+            }
+        }
+        return count;
+    }
+    public void dfs(int[][] isConnected, int[] visited, int i) {
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(isConnected, visited, j);
+            }
+        }
+    }
+}
