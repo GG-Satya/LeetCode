197bb71185ac7fc8fb2ce8a69224eb6171db50ed
GG-Satya <dsatyaprasanna@gmail.com>
2023-06-09 09:20:28 +0530

    9th june 2023, Daily LeetCode Challenge

---
+++ 744. Find Smallest Letter Greater Than Target/Solution.java
@@ -0,0 +1,11 @@
+// 744. Find Smallest Letter Greater Than Target
+// =============================================
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        for(char c: letters){
+            if(c > target)return c;
+        }
+        return letters[0];
+    }
+}
