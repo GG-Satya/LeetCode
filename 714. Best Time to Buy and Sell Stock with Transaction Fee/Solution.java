05f7cbb69ab920d4bdce18ce930e85c4e33089ea
GG-Satya <dsatyaprasanna@gmail.com>
2023-06-22 09:41:51 +0530

    22th june 2023, Daily LeetCode Challenge

---
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee/Solution.java
@@ -0,0 +1,15 @@
+// 714. Best Time to Buy and Sell Stock with Transaction Fee
+// =========================================================
+
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        if (prices.length <= 1) return 0;
+        int days = prices.length, buy[] = new int[days], sell[] = new int[days];
+        buy[0]=-prices[0];
+        for (int i = 1; i<days; i++) {
+            buy[i] = Math.max(buy[i - 1], sell[i - 1] - prices[i]); // keep the same as day i-1, or buy from sell status at day i-1
+            sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i] - fee); // keep the same as day i-1, or sell from buy status at day i-1
+        }
+        return sell[days - 1];
+    }
+}
