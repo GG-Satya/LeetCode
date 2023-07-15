290281fa95700394f9f29ced009a0b9c55be4331
GG-Satya <dsatyaprasanna@gmail.com>
2023-07-15 09:55:33 +0530

    15th july 2023, Daily LeetCode Challenge

---
+++ 1751. Maximum Number of Events That Can Be Attended II/Solution.java
@@ -0,0 +1,44 @@
+// 1751. Maximum Number of Events That Can Be Attended II
+// ======================================================
+
+class Solution {
+
+    private int[][] dp;
+
+    public int maxValue(int[][] events, int k) {
+         Arrays.sort(events, (a, b) -> a[0] - b[0]);
+        int n = events.length;
+        
+        dp = new int[k + 1][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        
+        return dfs(0, k, events);
+    }
+
+    private int dfs(int curIndex, int count, int[][] events) {
+        if (count == 0 || curIndex == events.length) {
+            return 0;
+        }
+        if (dp[count][curIndex] != -1) {
+            return dp[count][curIndex];
+        }
+        int nextIndex = bisectRight(events, events[curIndex][1]);
+        dp[count][curIndex] = Math.max(dfs(curIndex + 1, count, events), events[curIndex][2] + dfs(nextIndex, count - 1, events));
+        return dp[count][curIndex];
+    }
+
+    private static int bisectRight(int[][] events, int target) {
+        int left = 0, right = events.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (events[mid][0] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
