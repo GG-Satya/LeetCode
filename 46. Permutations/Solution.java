e49438999ffdde858d515903064546795128edcf
GG-Satya <dsatyaprasanna@gmail.com>
2023-08-02 12:15:15 +0530

    2nd August 2023, Daily LeetCode Challenge

---
+++ 46. Permutations/Solution.java
@@ -0,0 +1,23 @@
+// 46. Permutations
+// ================
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(new ArrayList<>(), ans, nums);
+        return ans;
+    }
+    private void backtrack(List<Integer> curr, List<List<Integer>> ans, int[] nums){
+        if(curr.size() == nums.length){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int num : nums){
+            if(!curr.contains(num)){
+                curr.add(num);
+                backtrack(curr, ans, nums);
+                curr.remove(curr.size() -1);
+            }
+        }
+    }
+}
