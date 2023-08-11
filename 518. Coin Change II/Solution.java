a623cb8a29ffcb158773e8799b21efc4d8ab681f
GG-Satya <dsatyaprasanna@gmail.com>
2023-08-11 15:01:43 +0530

    11th August 2023, Daily LeetCode Challenge

---
+++ 518. Coin Change II/Solution.java
@@ -0,0 +1,15 @@
+// 518. Coin Change II
+// ===================
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for(int coin : coins){
+            for(int i=coin; i<= amount; i++){
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+}
