369ddacbcbba57e6120e4aa15d7f08ba2d110f69
GG-Satya <dsatyaprasanna@gmail.com>
2023-08-21 22:19:32 +0530

    21st August 2023, Daily LeetCode Challenge

---
+++ 459. Repeated Substring Pattern/Solution.java
@@ -0,0 +1,9 @@
+// 459. Repeated Substring Pattern
+// ===============================
+
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String str = s + s;
+        return str.substring(1, str.length() - 1).contains(s);
+    }
+}
