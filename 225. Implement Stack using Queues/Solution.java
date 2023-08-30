3bd83c03f001e9a4aef12360a2c2c8cdc80beb0d
GG-Satya <dsatyaprasanna@gmail.com>
2023-08-30 09:08:37 +0530

    28th August 2023, Daily LeetCode Challenge

---
+++ 225. Implement Stack using Queues/Solution.java
@@ -0,0 +1,35 @@
+// 225. Implement Stack using Queues
+// =================================
+
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+         Arrays.sort(nums);
+        int n = nums.length;
+        int left = 0, right = nums[n - 1] - nums[0];
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // If there are enough pairs, look for a smaller threshold.
+            // Otherwise, look for a larger threshold.
+            if (countValidPairs(nums, mid) >= p) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+     private int countValidPairs(int[] nums, int threshold) {
+        int index = 0, count = 0;
+        while (index < nums.length - 1) {
+            // If a valid pair is found, skip both numbers.
+            if (nums[index + 1] - nums[index] <= threshold) {
+                count++;
+                index++;
+            }
+            index++;
+        }
+        return count;
+    }
+}
