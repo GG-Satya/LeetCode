e4038677e25735981053c3bae2d1408ccd90de9c
GG-Satya <dsatyaprasanna@gmail.com>
2023-09-06 22:52:38 +0530

    6th september 2023, Daily LeetCode Challenge

---
+++ 725. Split Linked List in Parts/Solution.java
@@ -0,0 +1,36 @@
+// 725. Split Linked List in Parts
+// ===============================
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+
+
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        ListNode[] parts = new ListNode[k];
+        int len = 0;
+        for (ListNode node = head; node != null; node = node.next)
+            len++;
+        int n = len / k, r = len % k; // n : minimum guaranteed part size; r : extra nodes spread to the first r parts;
+        ListNode node = head, prev = null;
+        for (int i = 0; node != null && i < k; i++, r--) {
+            parts[i] = node;
+            for (int j = 0; j < n + (r > 0 ? 1 : 0); j++) {
+                prev = node;
+                node = node.next;
+            }
+            prev.next = null;
+        }
+        return parts;
+    }
+}
