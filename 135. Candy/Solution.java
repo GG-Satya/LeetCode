975d8e89c4439b86d4dbe121f3ad5af696e812cb
GG-Satya <dsatyaprasanna@gmail.com>
2023-09-14 22:12:12 +0530

    13th september 2023, Daily LeetCode Challenge

---
+++ 135. Candy/Solution.java
@@ -0,0 +1,25 @@
+// 135. Candy
+// ==========
+
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies,1);
+
+        for(int i=1; i<n; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1]+1;
+            }
+        }
+
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = Math.max(candies[i], candies[i+1]+1);
+            }
+        }
+        int ans = 0;
+        while(--n >= 0) ans+= candies[n];
+        return ans;
+    }
+}
