28b7dbe88c097697af176a2fb01d2c00328a66a9
GG-Satya <dsatyaprasanna@gmail.com>
2023-09-21 12:10:54 +0530

    21st september 2023, Daily LeetCode Challenge

--- 4. Median of Two Sorted Arrays/Solution.java
+++ 4. Median of Two Sorted Arrays/Solution.java
@@ -1,28 +1,19 @@
 class Solution {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
-        int[] arr = new int[nums1.length+nums2.length];
-
-        merge(nums1,nums2,arr);
-
-        int mid = arr.length/2;
-
-        	return arr.length%2==0? (double) (arr[mid]+arr[mid-1])/2: arr[mid];
-        
-    }
-	// Merge sort logic 
-    private void merge(int[] a, int[] b, int[] c){
-
         int i=0,j=0,k=0;
+        int m = nums1.length , n = nums2.length, mid = (m+n)/2;
 
-        while(i< a.length && j<b.length){
+        int[] arr = new int[m+n];
 
-            if(a[i]<b[j]) c[k++]=a[i++];
-            else c[k++]= b[j++];
+        while(i<m && j<n){
+            if(nums1[i] < nums2[j])arr[k++] = nums1[i++];
+            else arr[k++] = nums2[j++];
         }
 
-        while(i<a.length) c[k++] = a[i++];
-        while(j<b.length) c[k++] = b[j++];
+        while(i<m)arr[k++] = nums1[i++];
+        while(j<n)arr[k++] = nums2[j++];
+
+        return (arr.length & 1) == 0 ? (double)(arr[mid]+arr[mid-1])/2: arr[mid];
     }
- }
+}
