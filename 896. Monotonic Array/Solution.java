674b0030fb02fc5dc7d4aa5fceed5d9633c404e0
GG-Satya <dsatyaprasanna@gmail.com>
2023-10-01 11:55:54 +0530

    29th september 2023, Daily LeetCode Challenge

---
+++ 896. Monotonic Array/Solution.java
@@ -0,0 +1,23 @@
+// 896. Monotonic Array
+// ====================
+
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false;
+            } else if (nums[i] < nums[i - 1]) {
+                increasing = false;
+            }
+
+            if (!increasing && !decreasing) {
+                return false; // Not monotonic, exit early
+            }
+        }
+
+        return true; // Monotonic
+    }
+}
