a70f2534b3030efa3e09b7797220593800942270
GG-Satya <dsatyaprasanna@gmail.com>
2023-10-01 11:54:45 +0530

    28th september 2023, Daily LeetCode Challenge

---
+++ 905. Sort Array By Parity/Solution.java
@@ -0,0 +1,14 @@
+// 905. Sort Array By Parity
+// =========================
+
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        for (int i = 0, j = 0; j < nums.length; j++)
+            if (nums[j] % 2 == 0) {
+                int tmp = nums[i];
+                nums[i++] = nums[j];
+                nums[j] = tmp;;
+            }
+        return nums;
+    }
+}
