35c01100ced2cf005e784eda48655203736e3831
GG-Satya <dsatyaprasanna@gmail.com>
2023-10-02 15:30:24 +0530

    2nd october 2023, Daily LeetCode Challenge

---
+++ 2038. Remove Colored Pieces if Both Neighbors are the Same Color/Solution.java
@@ -0,0 +1,17 @@
+// 2038. Remove Colored Pieces if Both Neighbors are the Same Color
+// ================================================================
+
+class Solution {
+    public boolean winnerOfGame(String colors) {
+        int a = 0, b = 0;
+        for(int i=1; i<colors.length()-1; i++){
+            if(colors.charAt(i) == colors.charAt(i-1) && colors.charAt(i) == colors.charAt(i+1)){
+                if(colors.charAt(i) == 'A')
+                    a++;
+                else
+                    b++;
+			}
+        }        
+        return a>b;
+    }
+}
