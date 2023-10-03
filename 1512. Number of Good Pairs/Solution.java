be03480a3d8ac355ef1037434f14dc7c5bf91973
GG-Satya <dsatyaprasanna@gmail.com>
2023-10-03 11:41:32 +0530

    3rd october 2023, Daily LeetCode Challenge

---
+++ 1512. Number of Good Pairs/Solution.java
@@ -0,0 +1,12 @@
+// 1512. Number of Good Pairs
+// ==========================
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int res = 0, count[] = new int[101];
+        for (int a: nums) {
+            res += count[a]++;
+        }
+        return res;
+    }
+}
