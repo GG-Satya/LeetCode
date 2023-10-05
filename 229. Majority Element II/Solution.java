2c940846790c11a958186f4bd2995ade4c8e84a8
GG-Satya <dsatyaprasanna@gmail.com>
2023-10-05 10:43:54 +0530

    5th october 2023, Daily LeetCode Challenge

---
+++ 229. Majority Element II/Solution.java
@@ -0,0 +1,53 @@
+// 229. Majority Element II
+// ========================
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        int candidate1 = 0, candidate2 = 1, count1 = 0, count2 = 0;
+
+        // First pass: Find potential candidates
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Second pass: Count occurrences of potential candidates
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        // Check if candidates are majority elements
+        if (count1 > nums.length / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > nums.length / 3) {
+            result.add(candidate2);
+        }
+
+        return result;
+    }
+}
