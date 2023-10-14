0c1c2e89265397276cf9d892ea1e8792dbd1145c
GG-Satya <dsatyaprasanna@gmail.com>
2023-10-14 15:32:20 +0530

    13th october 2023, Daily LeetCode Challenge

---
+++ 746. Min Cost Climbing Stairs/Solution.java
@@ -0,0 +1,17 @@
+// 746. Min Cost Climbing Stairs
+// =============================
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+	int first = cost[0];
+	int second = cost[1];
+	if (n<=2) return Math.min(first, second);
+	for (int i=2; i<n; i++) {
+		int curr = cost[i] + Math.min(first, second);
+		first = second;
+		second = curr;
+	}
+	return Math.min(first, second);
+    }
+}
