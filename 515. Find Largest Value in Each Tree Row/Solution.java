48bbc31242126f332e002d71f8661f5cc59ffc6d
GG-Satya <dsatyaprasanna@gmail.com>
2023-10-27 21:55:28 +0530

    24th october 2023, Daily LeetCode Challenge

---
+++ 515. Find Largest Value in Each Tree Row/Solution.java
@@ -0,0 +1,41 @@
+// 515. Find Largest Value in Each Tree Row
+// ========================================
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+       List<Integer> res = new ArrayList<Integer>();
+        helper(root, res, 0);
+        return res;
+    }
+    private void helper(TreeNode root, List<Integer> res, int d){
+        if(root == null){
+            return;
+        }
+       //expand list size
+        if(d == res.size()){
+            res.add(root.val);
+        }
+        else{
+        //or set value
+            res.set(d, Math.max(res.get(d), root.val));
+        }
+        helper(root.left, res, d+1);
+        helper(root.right, res, d+1);
+    }
+}
