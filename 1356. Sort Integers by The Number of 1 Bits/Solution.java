d9bc3ac49066a2349bee3fdaee19edca77e892f6
GG-Satya <dsatyaprasanna@gmail.com>
2023-10-30 22:04:47 +0530

    30th october 2023, Daily LeetCode Challenge

---
+++ 1356. Sort Integers by The Number of 1 Bits/Solution.java
@@ -0,0 +1,11 @@
+// 1356. Sort Integers by The Number of 1 Bits
+// ===========================================
+
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        for(int i=0;i<arr.length;i++)arr[i]+=Integer.bitCount(arr[i])*10001;
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++)arr[i]=arr[i]%10001;
+        return arr;
+    }
+}
