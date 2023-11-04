5bce8b9fa11084376ba7eb2a0e69f855a5f3fa8a
GG-Satya <dsatyaprasanna@gmail.com>
2023-11-04 21:42:25 +0530

    4th november 2023, Daily LeetCode Challenge

---
+++ 1503. Last Moment Before All Ants Fall Out of a Plank/Solution.java
@@ -0,0 +1,13 @@
+// 1503. Last Moment Before All Ants Fall Out of a Plank
+// =====================================================
+
+class Solution {
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int res = 0;
+        for (int i: left)
+            res = Math.max(res, i);
+        for (int i: right)
+            res = Math.max(res, n - i);
+        return res;
+    }
+}
