c3d8de6cc942e918c445e3a44a5a4e3e69dbc5cc
GG-Satya <dsatyaprasanna@gmail.com>
2023-11-24 22:42:09 +0530

    24th november 2023, Daily LeetCode Challenge

---
+++ 1561. Maximum Number of Coins You Can Get/Solution.java
@@ -0,0 +1,21 @@
+// 1561. Maximum Number of Coins You Can Get
+// =========================================
+
+class Solution {
+    public int maxCoins(int[] piles) {
+       Arrays.sort(piles);
+        ArrayDeque<Integer> queue = new ArrayDeque();
+        for (int num : piles) {
+            queue.addLast(num);
+        }
+        
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            queue.removeLast(); // alice
+            ans += queue.removeLast(); // us
+            queue.removeFirst(); // bob
+        }
+        
+        return ans; 
+    }
+}
