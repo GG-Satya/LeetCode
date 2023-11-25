ecbe5a5899b7ac69571211ba1e8c7672fcec28b4
GG-Satya <dsatyaprasanna@gmail.com>
2023-11-25 12:26:59 +0530

    25th november 2023, Daily LeetCode Challenge

---
+++ 1685. Sum of Absolute Differences in a Sorted Array/Solution.java
@@ -0,0 +1,29 @@
+// 1685. Sum of Absolute Differences in a Sorted Array
+// ===================================================
+
+class Solution {
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n];
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+        
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            int leftSum = prefix[i] - nums[i];
+            int rightSum = prefix[n - 1] - prefix[i];
+            
+            int leftCount = i;
+            int rightCount = n - 1 - i;
+            
+            int leftTotal = leftCount * nums[i] - leftSum;
+            int rightTotal = rightSum - rightCount * nums[i];
+            
+            ans[i] = leftTotal + rightTotal;
+        }
+        
+        return ans;
+    }
+}
