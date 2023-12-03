3f5e2970b5b6b66dd503ee18f7604d741132b137
GG-Satya <dsatyaprasanna@gmail.com>
2023-12-03 19:47:11 +0530

    3rd December 2023, Daily LeetCode Challenge

---
+++ 1266. Minimum Time Visiting All Points/Solution.java
@@ -0,0 +1,17 @@
+// 1266. Minimum Time Visiting All Points
+// ======================================
+
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int ans = 0;
+        for (int i = 0; i < points.length - 1; i++) {
+            int currX = points[i][0];
+            int currY = points[i][1];
+            int targetX = points[i + 1][0];
+            int targetY = points[i + 1][1];
+            ans += Math.max(Math.abs(targetX - currX), Math.abs(targetY - currY));
+        }
+        
+        return ans;
+    }
+}
