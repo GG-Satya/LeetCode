918c9d78ac0844ad2bba08770de0ab7e06466022
GG-Satya <dsatyaprasanna@gmail.com>
2023-12-19 22:29:47 +0530

    15th December 2023, Daily LeetCode Challenge

---
+++ 1436. Destination City/Solution.java
@@ -0,0 +1,24 @@
+// 1436. Destination City
+// ======================
+
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        for (int i = 0; i < paths.size(); i++) {
+            String candidate = paths.get(i).get(1);
+            boolean good = true;
+            
+            for (int j = 0; j < paths.size(); j++) {
+                if (paths.get(j).get(0).equals(candidate)) {
+                    good = false;
+                    break;
+                }
+            }
+
+            if (good) {
+                return candidate;
+            }
+        }
+        
+        return "";
+    }
+}
